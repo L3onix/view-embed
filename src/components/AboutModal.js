2de2ff220907e05c345288f4c240b65bb5859368
Leonardo Facundes <leonardo.fm00@gmail.com>
2020-03-03 18:10:13 -0300

    trabalhando no css

--- src/components/AboutModal.js
+++ src/components/AboutModal.js
@@ -1,7 +1,14 @@
 import React from 'react'
 import './AboutModal.css'
 
+import github from '../images/github.svg'
+import exit from '../images/exit.svg'
+
 export default class AboutModal extends React.Component {
+    onClose = e => {
+        this.props.show = false
+    }
+
     render() {
         if(!this.props.show){
             return null
@@ -9,7 +16,10 @@ export default class AboutModal extends React.Component {
         return (
             <div className="about-popup">
                 <div className="about-content">
-                    <span>View Embed</span>
+                    <img src={exit} alt="fechar popup about" id="exit" onClose={e => {this.onClose(e)}} />
+                    <span className="app-name">View Embed</span>
+                    <p></p>
+                    <a href="https://github.com/L3onix/view-embed" className="github"><img src={github} alt="logotipo do github" /></a>
                 </div>
             </div>
         )
