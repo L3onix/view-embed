c7e7b9d7055c73d36845df1354e3ce8489660550
Leonardo Facundes <Leonardo Facundes>
2020-03-25 18:19:47 -0300

    finalizando AboutModal incluindo css

--- src/components/AboutModal.js
+++ src/components/AboutModal.js
@@ -18,7 +18,16 @@ export default class AboutModal extends React.Component {
                 <div className="about-content">
                     <img src={exit} alt="fechar popup about" id="exit" onClick={this.onClose} />
                     <span className="app-name">View Embed</span>
-                    <p></p>
+                    <hr/>
+                    <div>
+                        <h2>Autor: Leonardo Facundes Moraes</h2>
+
+                        <h2>Propósito da aplicação</h2>
+                        <p>Esta aplicação permite que o usuário, a partir de um link de vídeo ou stream, possa gerar uma nova janela, com o player de vídeo embutido
+                            (<i>embed</i>), de forma simples e prática.</p>
+                        
+                        <h2>Desenvolvimento</h2>
+                    </div>
                     <a href="https://github.com/L3onix/view-embed" className="github"><img src={github} alt="logotipo do github" /></a>
                 </div>
             </div>
